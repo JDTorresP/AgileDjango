f9cfb6b08ad7e42f644af30c48a540f8a404a499
Hanner Andrés Ortiz Ramirez <ha.ortiz11@uniandes.edu.co>
2018-08-19 16:34:14 -0500

    Se Incluye en la tabla clip_media el idusuario como foranea y se incluye la tabla en el manejador de tablas del administrador.

--- gallery/admin.py
+++ gallery/admin.py
@@ -8,7 +8,9 @@ from gallery.models import Media
 
 from gallery.models import User
 from .models import Clip
+from .models import Clip_Media
 
 admin.site.register(Clip)
 admin.site.register(Media)
 admin.site.register(User)
+admin.site.register(Clip_Media)
