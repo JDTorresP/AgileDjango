0fe0104e03fd284b47db7a7a3c312e30576f50f3
Danny <dannylhurtado@gmail.com>
2018-08-19 23:02:27 -0500

    Cambios menores

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 
 # Register your models here.
 from gallery.models import Media
-
 from gallery.models import User
-from .models import Clip
-
-from .models import Category
-from .models import Clip_Media
+from gallery.models import Clip
+from gallery.models import Category
+from gallery.models import Clip_Media
 
 admin.site.register(Clip)
 admin.site.register(Media)
 admin.site.register(User)
-
 admin.site.register(Category)
-
 admin.site.register(Clip_Media)
