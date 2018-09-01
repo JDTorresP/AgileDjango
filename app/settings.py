9a43ebc7539bbf3657f2600b44d80211cd5cfa7f
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-09-01 00:18:32 -0500

    Configuración general de la base de datos,

--- app/settings.py
+++ app/settings.py
@@ -82,41 +82,41 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {'default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config()}
 
-DATABASES = {
-    #Local
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'gallery',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-    }
-
-    #Amazon
-
-    # 'default': {
-    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #    'NAME': 'd4eg5rsuf9ehh3',
-    #    'USER': 'qwkzxcnsoaqkgy',
-    #     'PASSWORD': '7863dc3566996195d0830dda52bc3e05522287701cdacdf02d3e3151a1029a7c',
-    #     'HOST': 'ec2-54-83-13-119.compute-1.amazonaws.com',
-    #     'PORT': '5432'
-    # }
-
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'agile',
-    #     'USER': 'dbuser',
-    #     'PASSWORD': 'casa123',
-    #     'HOST': '',
-    #     'PORT': '',
-    # }
-
-}
+# DATABASES = {
+#     #Local
+#     #  'default': {
+#     #     'ENGINE': 'django.db.backends.mysql',
+#     #     'NAME':'gallery',
+#     #     'USER':'root',
+#     #     'PASSWORD':'root',
+#     #     'HOST':'127.0.0.1',
+#     #     'PORT':'3306',
+#     # }
+#
+#     #Amazon
+#
+#     # 'default': {
+#     #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     #    'NAME': 'd4eg5rsuf9ehh3',
+#     #    'USER': 'qwkzxcnsoaqkgy',
+#     #     'PASSWORD': '7863dc3566996195d0830dda52bc3e05522287701cdacdf02d3e3151a1029a7c',
+#     #     'HOST': 'ec2-54-83-13-119.compute-1.amazonaws.com',
+#     #     'PORT': '5432'
+#     # }
+#
+#
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     #     'NAME': 'agile',
+#     #     'USER': 'dbuser',
+#     #     'PASSWORD': 'casa123',
+#     #     'HOST': '',
+#     #     'PORT': '',
+#     # }
+#
+# }
 
 
 # Password validation
